498f6cd8d74290ba6be50884d068b6dae024ea0c
Tea <icepie.dev@gmail.com>
2022-11-03 19:46:49 +0800

    test struct

--- src/gw.rs
+++ src/gw.rs
@@ -1,3 +1,26 @@
+pub use reqwest::Client as HttpClient;
+use core::fmt::Result;
+// Result
+
+
+
 pub struct GW {
     client: HttpClient
+}
+
+impl GW {
+    pub fn new(client: HttpClient) -> Self {
+        GW { client:client}
+    }
+
+    pub fn test(&self, str: String)  {
+
+    // let res = self.client.get(str)
+    // .body("the exact body that is sent")
+    // .send();
+
+    
+    println!("{:?}", str);
+   
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use crate::gw::GW;
+
 pub mod gw;
 
+
 fn main() {
+
+    let client = reqwest::Client::new();
+
+    let gw = GW::new(client);
+
+
+    gw.test("https://www.baidu.com".to_string());
+
+
     println!("Hello, world!");
 }
